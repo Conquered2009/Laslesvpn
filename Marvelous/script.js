d6a6a3ae337192bc1374c37d5537800426adebfa
Conquered Ngbede <conqueredn@gmail.com>
2025-12-04 13:26:41 +0100

    Javascript

---
+++ Marvelous/script.js
@@ -0,0 +1,105 @@
+// function displayI() {
+//     document.getElementById("fname").innerHTML = "Conquered";
+//     document.getElementById("lname").innerHTML = "Ngbede";
+// }
+// // garbbing and storing the HTML elements in a varialbe
+// let h1Elem = document.getElementById("h1text");
+// let h2Elem = document.getElementById("h2text");
+
+
+// h1Elem.innerHTML = "My blog title";
+// h1Elem.style.color =  "Blue";
+
+// h2Elem.innerHTML = "The Kidnapping Pandemic";
+
+// // grab input element
+// let inputfN = document.getElementById("firstName").value;
+// let inputlN = document.getElementById("lastName").value;
+// let spanElem = document.getElementById("showFN").value;
+
+// console.log(inputFN);
+// console.log(inputFN);
+
+// spanElem.style.color = "red";
+// spanElem.style.fontweight= "900";
+// // let newLNValue = inputFN.value;
+// // let newLNValue = inputLN.value;
+
+// function displayFullName() {
+//     let fullName = inputFN.value +" "+ inputLN.value;
+//     spanElem.innerHTML = fullName;
+// }
+
+
+// function displayIN() {
+//     document.getElementById("Inumber").innerHTML = "Conquered";
+//     document.getElementById("Inumber").innerHTML = "Ngbede";
+// }
+// // garbbing and storing the HTML elements in a varialbe
+// let h1Elem = document.getElementById("h1text");
+// let h2Elem = document.getElementById("h2text");
+
+
+// h1Elem.innerHTML = "My blog title";
+// h1Elem.style.color =  "Blue";
+
+// h2Elem.innerHTML = "The Kidnapping Pandemic";
+
+// // grab input element
+// let inputIN = document.getElementById("firstName").value;
+// let inputIN = document.getElementById("lastName").value;
+// let spanElem = document.getElementById("showFN").value;
+
+// console.log(inputFN);
+// console.log(inputFN);
+
+// spanElem.style.color = "red";
+// spanElem.style.fontweight= "900";
+// // let newLNValue = inputFN.value;
+// // let newLNValue = inputLN.value;
+
+// function displayFullName() {
+//     let fullName = inputFN.value +" "+ inputLN.value;
+//     spanElem.innerHTML = fullName;
+// }
+
+// ler
+
+
+
+let h1Elem = document.getElementById("h1text");
+let h2Elem = document.getElementById("h2text");
+
+
+// grabbing and storing the HTML elements in a variable
+h1Elem.innerHTML = "My Blog Title";
+h1Elem.style.color = "blue";
+h1Elem.style.textAlign = "center";
+
+h2Elem.innerHTML = "The Kidnapping Pandemic!";
+"900";
+
+// grab input elements
+
+let inputFN = document.getElementById("firstName");
+let inputLN = document.getElementById("lastName");
+let spanElem = document.getElementById("showFN");
+
+spanElem.style.color = "blue";
+spanElem.style.fontWeight = 900;
+
+function displayFullName() {
+    let fullName = inputFN.value + " " + inputLN.value;
+    spanElem.innerHTML = fullName;
+}
+
+// grab number elements
+
+let number1 = document.getElementById("num1");
+let number2 = document.getElementById("num2");
+let spanNum = document.getElementById("numResult");
+
+function displayResult() {
+    let Result = Number(number1.value) + Number(number2.value);
+    spanNum.innerHTML = submit
+}
